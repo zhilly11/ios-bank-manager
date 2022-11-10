4d63847f3d7539723909d00d4b3d15548f83f1e6
미니 <cow970814@naver.com>
2022-11-10 20:36:44 +0900

    feat : 재사용 ScrollView 구현 및 화면 추가

---
+++ BankManagerUIApp/BankManagerUIApp/CustomerLineScrollView.swift
@@ -0,0 +1,56 @@
+//
+//  CustomerLineScrollView.swift
+//  BankManagerUIApp
+//
+//  Copyright (c) 2022 Minii All rights reserved.
+
+import UIKit
+
+class CustomerLineScrollView: UIScrollView {
+    private let contentStackView: UIStackView = {
+        let stackView = UIStackView()
+        
+        for _ in 0...20 {
+            let label = UILabel()
+            
+            label.text = "example"
+            label.textColor = .label
+            label.textAlignment = .center
+            label.font = .preferredFont(forTextStyle: .title3)
+            label.adjustsFontForContentSizeCategory = true
+            stackView.addArrangedSubview(label)
+        }
+        
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 20
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func configureUI() {
+        showsVerticalScrollIndicator = false
+        showsHorizontalScrollIndicator = false
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(contentStackView)
+        contentStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentStackView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            contentStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            contentStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            contentStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            contentStackView.widthAnchor.constraint(equalTo: widthAnchor)
+        ])
+    }
+}
--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -12,6 +12,9 @@ final class ViewController: UIViewController {
     private let workTimeLabel = WorkTimeLabelStackView()
     private let queueLabel = QueueLabelStackView()
     
+    let waitingLineScrollView = CustomerLineScrollView()
+    let workingLineScrollView = CustomerLineScrollView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,13 +26,17 @@ final class ViewController: UIViewController {
         makeConstraintTopButtonView()
         makeConstraintWorkTimeLabel()
         makeConstraintQueueLabel()
+        makeConstraintWaitingScrollView()
+        makeConstraintWorkingScrollView()
     }
     
     private func addSubViews() {
         [
             userTopButtonView,
             workTimeLabel,
-            queueLabel
+            queueLabel,
+            waitingLineScrollView,
+            workingLineScrollView
         ].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
             view.addSubview($0)
@@ -58,6 +65,26 @@ final class ViewController: UIViewController {
             queueLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             queueLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             queueLabel.topAnchor.constraint(equalTo: workTimeLabel.bottomAnchor, constant: 8)
+        ])
+    }
+    
+    private func makeConstraintWaitingScrollView() {
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            waitingLineScrollView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            waitingLineScrollView.topAnchor.constraint(equalTo: queueLabel.bottomAnchor),
+            waitingLineScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            waitingLineScrollView.trailingAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+
+    private func makeConstraintWorkingScrollView() {
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            workingLineScrollView.leadingAnchor.constraint(equalTo: view.centerXAnchor),
+            workingLineScrollView.topAnchor.constraint(equalTo: queueLabel.bottomAnchor),
+            workingLineScrollView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            workingLineScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
 }
